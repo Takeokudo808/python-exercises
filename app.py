c7ad6fac6f7a1db51b94bf403369abd61c5d586f
Shinsou <takeokudo808@gmail.com>
2018-08-14 18:30:51 -1000

    Finished Question 1

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 # 1. As a valued customer at the Bank of Honolulu, you make a deposit of $1000. Your savings account balance prior to the deposit has an amount of $8000. Calculate the new savings account balance. Print the new savings account balance and concatenate the dollar sign.
+deposit= 1000
+balance= 8000
+def add(num1, num2):
+    return num1 + num2
+add(deposit, balance)
+total= add(deposit, balance)
+print('$' + str(total))
+
 
 # 2.You need to pay taxes on the $500 cash prize that you won to the IRS ( The tax rate is 30%). Calculate the tax amount and subtract this from your savings balance. Print the updated savings account balance and concatenate the dollar sign.
-
 # 3. The savings account accrues an annual interest rate of 2%. Calculate the interest earned for the first quarter of 2018, using the original account balance from Question 1. Print the interst earned in the first quarter and concatenate the dollar sign.
 
 # 4. Function add
